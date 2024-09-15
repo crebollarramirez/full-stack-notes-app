65ef6f102aa897a50826b36db572df07ad49cc24
Chris <christopherrebollar0@icloud.com>
2024-09-14 20:41:59 -0700

    adding new files and worked on form.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,37 @@ import Home from "./pages/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 import NotFound from "./pages/NotFound"
 
-function App() {
-
-  return (
-    <>
-    </>
-  )
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
 }
 
-export default App
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/logout" element={<Logout />}/>
+          <Route path="/register" element={<RegisterAndLogout />}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes> 
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
