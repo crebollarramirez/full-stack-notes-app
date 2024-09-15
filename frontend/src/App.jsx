b8f4d5c684fc0d16a4ed5463d8fb36ab8a1ea9b4
Chris <christopherrebollar0@icloud.com>
2024-09-14 20:41:54 -0700

    worked on app.jsx, protected route, and form.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
-import react from "react"
+import react from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./pages/NotFound"
 
 function App() {
 
