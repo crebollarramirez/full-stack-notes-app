adea23c09f9a1eaef6e0e8804ef7f5a6a29987cf
Chris <christopherrebollar0@icloud.com>
2024-09-15 21:19:03 -0700

    adding loading indicator and styles for the home and note

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,10 @@
+import "../styles/loadingIndicator.css"
+
+const LoadingIndictor = () => {
+    return <div className="loading-container">
+        <div className="loader">
+        </div>
+    </div>
+}
+
+export default LoadingIndictor;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../api";
+import Note from "../components/Note"
+import "../styles/home.css"
 
 function Home() {
   const [notes, setNotes] = useState([]);
@@ -8,17 +10,78 @@ function Home() {
 
   useEffect(() => {
     getNotes();
-  }, [])
+  }, []);
 
   const getNotes = () => {
     api
       .get("./api/notes/")
       .then((res) => res.data)
-      .then((data) => {setNotes(data); console.log(data)})
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
       .catch((err) => alert(err));
   };
 
-  return <div>Home</div>;
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note was deleted!");
+        else alert("Failed to delete note.");
+        getNotes();
+      })
+      .catch((error) => alert(error));
+    
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) alert("Note created!");
+        else alert("Failed to make note.");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+    
+  };
+
+  return (
+    <>
+      <div>
+        <h2>Notes</h2>
+        {notes.map((note) => <Note note={note} onDelete={deleteNote} key={note.id}/>)}
+      </div>
+
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br/>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <br/>
+        <label htmlFor="content">Content:</label>
+        <br/>
+        <textarea
+          id="content"
+          name="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br></br>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </>
+  );
 }
 
 export default Home;
