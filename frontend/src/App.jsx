59d0308e484cbcf879d91dac7a769b12089204d1
Chris <christopherrebollar0@icloud.com>
2024-09-15 20:31:53 -0700

    allowing front end and backend communication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import react from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import ProtectedRoute from "./components/ProtectedRoute";
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function Logout() {
   localStorage.clear();
@@ -18,24 +18,22 @@ function RegisterAndLogout() {
 
 function App() {
   return (
-    <>
       <BrowserRouter>
         <Routes>
           <Route
             path="/"
-            element={
+              element={
               <ProtectedRoute>
                 <Home />
               </ProtectedRoute>
             }
           />
-          <Route path="/login" element={<Login />}/>
-          <Route path="/logout" element={<Logout />}/>
-          <Route path="/register" element={<RegisterAndLogout />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
           <Route path="*" element={<NotFound />} />
-        </Routes> 
+        </Routes>
       </BrowserRouter>
-    </>
   );
 }
 
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
 import api from "../api";
-import { useNavigation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
 
 function Form({ route, method }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigation();
+  const navigate = useNavigate();
 
   const name = method === "login" ? "Login" : "Register";
+
   const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
 
-    try{
-        const res = await api.post(route, {username, password})
-        if (method === "login"){
-            localStorage.setItem(ACCESS_TOKEN, res.data.access);
-            localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-            navigate("/")
-        }else{
-            navigate("/login")
-        }
-    }catch(error){
-        alert(error)
-    }finally{
-        setLoading(false)
+    try {
+      const res = await api.post(route, { username, password });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="form-container">
       <h1>{name}</h1>
       <input
         className="form-input"
@@ -40,7 +42,6 @@ function Form({ route, method }) {
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Username"
       />
-
       <input
         className="form-input"
         type="password"
@@ -48,10 +49,11 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
-
       <button className="form-button" type="submit">
         {name}
       </button>
     </form>
   );
 }
+
+export default Form;
